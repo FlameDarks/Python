28901a821e142c7c805958ed457c733177783bc8
FlameDarks <842356712@qq.com>
2019-12-17 10:10:59 +0800

    1

--- day7.py
+++ day7.py
@@ -4,3 +4,8 @@ def download():
     bs=rs.content
     with open("bd.png","wb") as f:
         f.write(bs)
+def download2():
+    rs = requests.get("http://wvioce.spriteapp.cn/voice/2016/0904/57cb989b1e3b4.mp3")
+    bs = rs.content
+    with open("first.mp3","wb") as f:
+        f.write(bs)
